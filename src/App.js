4f681f2c8f4087c7f4487b12a27263821c9d715f
suchov <artem.suchov@gmail.com>
2018-11-08 17:33:27 +0200

    fix by Roger's suggestion - refactor Class to a function cause I'm not using state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
 import {Route} from 'react-router-dom';
 
-class BooksApp extends Component {
-  render() {
-    return (
-      <div className="app" data-test="component-app">
-        <Route exact path='/' component={ListBooks}/>
-        <Route path='/search' component={SearchBooks}/>
-      </div>
-    )
-  }
+const BooksApp = () => {
+  return (
+    <div className="app" data-test="component-app">
+      <Route exact path='/' component={ListBooks}/>
+      <Route path='/search' component={SearchBooks}/>
+    </div>
+  )
 }
 
-export default BooksApp
+export default BooksApp;
